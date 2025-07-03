50ba46b843f6e722824f01ab584ae36a677a4bb0
Ranjan Prasad <ranjanprasad@Ranjans-MacBook-Air.local>
2025-07-03 22:16:58 +0530

    TechNova

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "../src/components/Header/Header";
+import Footer from "../src/components/Footer/Footer";
+import Hero from "../src/components/Hero/Hero";
+import Approach from "../src/components/Approach/Approach";
+import Testimonials from "../src/components/Testimonials/Testimonials";
+import Projects from "../src/components/Projects/Projects";
+import Contacts from "../src/components/Contacts/Contacts";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Projects />
+      <Approach />
+      <Testimonials />
+      <Contacts />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
